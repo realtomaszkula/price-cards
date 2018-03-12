47e120fbafaee3fe369ee78c1ec6c599e3aece7d
Tomasz Kula <tomaszkula07@gmail.com>
2018-03-12 12:30:13 +0100

    Add @angular/animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
 
 import { AppComponent } from './app.component';
 import { PriceCardsComponent } from './price-cards/price-cards.component';
 import { PriceCardComponent } from './price-card/price-card.component';
 
-
 @NgModule({
-  declarations: [
-    AppComponent,
-    PriceCardsComponent,
-    PriceCardComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, PriceCardsComponent, PriceCardComponent],
+  imports: [BrowserModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
